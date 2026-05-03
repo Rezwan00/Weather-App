af6d721910147e2b0682579b09d9d894504a1a68
Rezwan00 <ahmadrezwanqaderi@gmail.com>
2026-05-03 19:44:34 +0300

    feat: updated forecast cards, and polish UI

--- src/components/ForecastList/ForecastList.tsx
+++ src/components/ForecastList/ForecastList.tsx
@@ -1,5 +1,5 @@
 import type { TemperatureUnit } from "../../api/weatherApi";
-import  { getWeatherInfo } from "../../utils/weatherCode";
+import { getWeatherInfo } from "../../utils/weatherCode";
 import styles from "./ForecastList.module.scss";
 
 type Props = {
@@ -15,21 +15,43 @@ type Props = {
 export function ForecastList({ daily, unit }: Props) {
   return (
     <section className={styles.grid}>
-     {daily.time.map((date, index) => {
-  const weatherInfo = getWeatherInfo(daily.weather_code[index]);
+      {daily.time.map((date, index) => {
+        const weatherInfo = getWeatherInfo(daily.weather_code[index]);
 
-  return (
-    <article key={date} className={styles.day}>
-      <h3>{new Date(date).toLocaleDateString()}</h3>
+        const weekday = new Date(date).toLocaleDateString(undefined, {
+          weekday: "short",
+        });
 
-      <div className={styles.icon}>{weatherInfo.icon}</div>
+        const formattedDate = new Date(date).toLocaleDateString(undefined, {
+          day: "2-digit",
+          month: "2-digit",
+        });
 
-      <p>{weatherInfo.label}</p>
-      <p>Max: {Math.round(daily.temperature_2m_max[index])}°{unit === "celsius" ? "C" : "F"}</p>
-      <p>Min: {Math.round(daily.temperature_2m_min[index])}°{unit === "celsius" ? "C" : "F"}</p>
-    </article>
-  );
-})}
+        return (
+          <article key={date} className={styles.day}>
+            <div className={styles.header}>
+              <span className={styles.weekday}>{weekday}</span>
+              <span className={styles.date}>{formattedDate}</span>
+            </div>
+
+            <div className={styles.temps}>
+              <span className={styles.max}>
+                {Math.round(daily.temperature_2m_max[index])}°
+                {unit === "celsius" ? "C" : "F"}
+              </span>
+
+              <span className={styles.min}>
+                {Math.round(daily.temperature_2m_min[index])}°
+                {unit === "celsius" ? "C" : "F"}
+              </span>
+            </div>
+
+            <div className={styles.icon}>{weatherInfo.icon}</div>
+
+            <p>{weatherInfo.label}</p>
+          </article>
+        );
+      })}
     </section>
   );
 }
